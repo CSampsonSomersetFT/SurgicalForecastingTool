38a2709bc4411678ad8049958cda607f0b6573b6
Chris Sampson <christopher.sampson@somersetft.nhs.uk>
2025-08-07 11:09:22 +0100

    Updating structure

--- SQL_queries/referrals_general_surgery.sql
+++
@@ -1,8 +0,0 @@
-SELECT RefRecDate, Urgency, COUNT(*) as num_refs
-  FROM [ReportingViews].[Referrals].[vwReferrals_TEST]
-  WHERE (Lead_Clinician_Title is not null or Profile_Service LIKE '%Surgery%' OR Profile_Service NOT LIKE '%Endoscopy%')
-  AND TreatFuncCode='100'
-  AND DATEPART(year, GETDATE()) - DATEPART(year, RefRecDate) < 5
-
-  GROUP BY RefRecDate, Urgency
-  ORDER BY RefRecDate
